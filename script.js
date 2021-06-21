346aec2e9865711e8b89ec4fdac5636b0063df7c
Matthew <furikuribeebop@gmail.com>
2021-06-21 17:36:12 +0930

    added JS to restore navbar on resize

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//toggle nav in mobile view
 function toggleNav() {
     var nav = document.getElementById("links");
     if(nav.style.display == "none") {
@@ -6,3 +7,13 @@ function toggleNav() {
         nav.style.display = "none";
     }  
 };
+
+//listen for window resize event to restore navbar in case of resize
+window.addEventListener('resize', function(event){
+
+    if (this.window.innerWidth > 1000) {
+        document.getElementById("links").style.display = "flex";
+    } else {
+        document.getElementById("links").style.display = "none";
+    }
+});
