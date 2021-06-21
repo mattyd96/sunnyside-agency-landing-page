717fa0ed24ed0c19bf59b1a93252200e29d30433
Matthew <furikuribeebop@gmail.com>
2021-06-21 17:25:17 +0930

    mobile layout finished

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    var nav = document.getElementById("links");
+    if(nav.style.display == "none") {
+        nav.style.display = "flex";
+    } else {
+        nav.style.display = "none";
+    }  
+};
